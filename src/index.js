e52f99792c1f65274e2d9adbb238c2d0574eccf6
Arkadz <penkratpavel@gmail.com>
2018-10-07 23:17:35 +0300

    !

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 module.exports = function longestConsecutiveLength(array) {
-  array.sort((a, b) => a - b);
-  let maxConsecutiveLength = 1;
-  let temp = 1;
-  if (array.length === 0) return 0;
-  if (array.length === 1) return 1;
-  for (let i = 1; i < array.length; i++) {
-    if (array[i - 1] + 1 === array[i]) maxConsecutiveLength += 1;
-    else {
-      if (temp < maxConsecutiveLength) temp = maxConsecutiveLength;
-      maxConsecutiveLength = 1;
+  
+  if(array.length == 0) {
+    return 0;
+  }
+  var info = {current: null, currentConsLength : 1, maxConsLength : 1};
+
+
+  var set = new Set(array);
+  //for (let item of set.values()) console.log(item);
+
+  var countConsLengthWithSet = function(set, info) {
+    for (let item of set.values()) {
+      var currentNumber;
+      info.current = item;
+      currentNumber = info.current - 1;
+      set.delete(item)
+      while(set.has(currentNumber)) {
+        info.currentConsLength++;
+        set.delete(currentNumber--);
+      }
+      currentNumber = info.current + 1;
+      while(set.has(currentNumber)) {
+        info.currentConsLength++;
+        set.delete(currentNumber++);
+      }
+      if(info.currentConsLength > info.maxConsLength) {
+        info.maxConsLength = info.currentConsLength;
+      }
+      info.currentConsLength = 1;
+      break;
     }
   }
-  return temp;
+
+  while(set.size != 0) {
+    //console.log(set.length);
+    countConsLengthWithSet(set, info);
+    
+  }
+  return info.maxConsLength;
+
+  
 }
+
+
+
